70e39e76ac9ad53efa7c873af6cca9e01fe632ca
Nasir Ahmed <ahsannasirahmed@gmail.com>
2025-10-28 18:01:43 +0600

    state update

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -3,29 +3,37 @@ import { useState } from "react";
 import SearchTask from "./SearchTAsk";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 export default function TaskBoard() {
   const defaultTasks = {
     id: crypto.randomUUID(),
-    title: "Learn React ",
+    title: "Learn React Native ",
     "description ":
       "I want to learn react such than I can treat it like my slave and make it d whatever I want to do",
     tags: ["web", " react", "js"],
     priority: "high",
     isFavorite: true,
   };
-  const [tasks, setTasksa] = useState([defaultTasks]);
+  const [tasks, setTasks] = useState([defaultTasks]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask() {
+    console.log("Add Task Clicked");
+  }
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal />}
+
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
-          <TaskList />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
