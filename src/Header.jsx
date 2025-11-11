5083e791841caa72c93133c234998d7caf1fa85a
Nasir Ahmed <ahsannasirahmed@gmail.com>
2025-11-11 17:31:05 +0600

    delete task , search tearm,delete all, no task found added

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import LWSLogo from "./assets/nasir.png";
 
 export default function Header() {
   return (
-    <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
+    <nav className="py-6 md:py-8 fixed top-0 w-full  z-50">
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         <a href="/">
           <img className="h-[100px]" src={LWSLogo} alt="Lws" />
--- src/task/TaskList.jsx
+++ src/task/TaskList.jsx
@@ -1,6 +1,6 @@
 import { FaStar } from "react-icons/fa";
 
-export default function Tasklist({ tasks, onEdit }) {
+export default function TaskList({ tasks, onEdit, onDelete, onFav }) {
   return (
     <div className="overflow-auto">
       <table className="table-fixed overflow-auto xl:w-full">
@@ -32,15 +32,17 @@ export default function Tasklist({ tasks, onEdit }) {
         <tbody>
           {tasks.map((task) => (
             <tr
-              key={tasks.id}
+              key={task.id}
               className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
             >
               <td>
-                {task.isFavorite ? (
-                  <FaStar color="Yellow" />
-                ) : (
-                  <FaStar color="gray" />
-                )}
+                <button onClick={() => onFav(task.id)}>
+                  {task.isFavorite ? (
+                    <FaStar color="yellow" />
+                  ) : (
+                    <FaStar color="gray" />
+                  )}
+                </button>
               </td>
               <td>{task.title}</td>
               <td>
@@ -60,8 +62,18 @@ export default function Tasklist({ tasks, onEdit }) {
               <td className="text-center">{task.priority}</td>
               <td>
                 <div className="flex items-center justify-center space-x-3">
-                  <button className="text-red-500">Delete</button>
-                  <button className="text-blue-500">Edit</button>
+                  <button
+                    className="text-red-500"
+                    onClick={() => onDelete(task.id)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className="text-blue-500"
+                    onClick={() => onEdit(task)}
+                  >
+                    Edit
+                  </button>
                 </div>
               </td>
             </tr>
@@ -71,5 +83,91 @@ export default function Tasklist({ tasks, onEdit }) {
     </div>
   );
 }
-// module 3
-// Md Nasir Ahmed
+
+// import { FaStar } from "react-icons/fa";
+
+// export default function Tasklist({ tasks, onEdit, onDelete, onFav }) {
+//   return (
+//     <div className="overflow-auto">
+//       <table className="table-fixed overflow-auto xl:w-full">
+//         <thead>
+//           <tr>
+//             <th className="p-4 pb-8 text-sm font-semibold capitalize w-[48px]"></th>
+//             <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">
+//               {" "}
+//               Title{" "}
+//             </th>
+//             <th className="p-4 pb-8 text-sm font-semibold capitalize w-full">
+//               {" "}
+//               Description{" "}
+//             </th>
+//             <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">
+//               {" "}
+//               Tags{" "}
+//             </th>
+//             <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+//               {" "}
+//               Priority{" "}
+//             </th>
+//             <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+//               {" "}
+//               Options{" "}
+//             </th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {tasks.map((task) => (
+//             <tr
+//               key={tasks.id}
+//               className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
+//             >
+//               <td>
+//                 <button onClick={() => onFav(task.id)}>
+//                   {task.isFavorite ? (
+//                     <FaStar color="Yellow" />
+//                   ) : (
+//                     <FaStar color="gray" />
+//                   )}
+//                 </button>
+//               </td>
+//               <td>{task.title}</td>
+//               <td>
+//                 <div>{task.description}</div>
+//               </td>
+//               <td>
+//                 <ul className="flex justify-center gap-1.5 flex-wrap">
+//                   {task.tags.map((tag) => (
+//                     <li key={tag}>
+//                       <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
+//                         {tag}
+//                       </span>
+//                     </li>
+//                   ))}
+//                 </ul>
+//               </td>
+//               <td className="text-center">{task.priority}</td>
+//               <td>
+//                 <div className="flex items-center justify-center space-x-3">
+//                   <button
+//                     className="text-red-500"
+//                     onClick={() => onDelete(task.id)}
+//                   >
+//                     Delete
+//                   </button>
+//                   <button
+//                     className="text-blue-500"
+//                     onClick={() => onEdit(task)}
+//                   >
+//                     Edit
+//                   </button>
+//                 </div>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
+// // module 3
+// // Md Nasir Ahmed
